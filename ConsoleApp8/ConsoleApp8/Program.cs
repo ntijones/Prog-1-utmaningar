7c8014d5cbaa2f12f5f361583c082bed827cfefd
ntijones <31617373+ntijones@users.noreply.github.com>
2018-05-21 09:49:54 +0200

    Initial commit

---
+++ ConsoleApp8/ConsoleApp8/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            meny();
+            Console.ReadLine();
+        }
+        static void meny() //Skriver ut de alternativ som finns att välja på 
+        {
+            Console.WriteLine("Meningskonveryerare");
+            Console.WriteLine("Välj 1 för att konvertera till stora bokstäver");
+            Console.WriteLine("Välj 2 för att konvertera till små bokstäver");
+            Console.Write("Ditt val:");
+            string userInput = Console.ReadLine(); //Gör så att programmet inte kraschar
+
+            switch (userInput) //Skapar en meny för de alternativ som finns, uppercase och lowercase.
+            {
+                case "1": //Caps
+                    upperCase(); 
+                    break;
+                case "2": //Små bokstäver
+                    lowerCase();
+                    break;
+                default:
+                    Console.WriteLine("Felaktigt input, försök igen!"); meny(); //Om användaren skriver in fel, körs denna kod
+                    break;
+            }
+        }
+        static void upperCase() 
+        {
+            Console.Write("Skriv din mening: ");
+            //Gör små bokstäver till caps
+            string input = Console.ReadLine().ToUpper();
+            Console.WriteLine("Din mening:" + input);
+        }
+
+        static void lowerCase()
+        {
+            Console.Write("Skriv din mening: ");
+            //Gör om caps till små bokstäver
+            string input = Console.ReadLine().ToLower();
+            Console.WriteLine("Din mening:" + input);
+        }
+    }
+}
---
+++ Simpel kalkylator Jones Al Zubaidi/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    
+    class Program
+    {
+        
+        
+        static void Main(string[] args)
+        {
+            while (true) //Skapar en while-loop. 
+            {
+                Menu();
+                int val = GetInput();
+                Räkna(val);
+                Console.WriteLine();
+            }
+        }
+
+        static void Menu() // Skriver ut text och alternativen som finns att välja på 
+        {
+            Console.WriteLine("Simpel kalkylator");
+            Console.WriteLine("Välj vilket räknesätt du vill använda");
+            Console.WriteLine("Räknesätt:");
+            Console.WriteLine("1. Addition");
+            Console.WriteLine("2. Subtration");
+            Console.WriteLine("3. Multiplikation");
+            Console.Write("Ditt val: "); //Användaren får skriva in ett alternativ (Addition, subration och multiplikation)
+        }
+
+        static int GetInput() 
+        {
+            // Översätter sträng till en int och returnerar värdet
+            return int.Parse(Console.ReadLine());
+        }
+
+        private static void Räkna(int val)
+        {
+            switch(val) 
+            { //Koden känner efter vilket tal användaren har angett. 
+                case 1:
+                    Console.WriteLine(Addition()); //
+                    break;
+
+                case 2:
+                    Console.WriteLine(Subration()); // 
+                    break;
+
+                case 3:
+                    Console.WriteLine(Multiplikation()); // 
+                    break;
+
+                default:
+                    Console.WriteLine("Felaktigt input"); //
+                    break;
+            }
+                
+        }
+
+        static int Addition() //Skapar sektion för addition
+        {
+            Console.Write("Tal 1: ");
+            int tal1 = GetInput();
+            Console.Write("Tal 2: ");
+            int tal2 = GetInput();
+            Console.Write("Summan blir: ");
+            return tal1 + tal2;
+        }
+
+        static int Subration() //Skapar sektion för subration
+        {
+            Console.Write("Tal 1: ");
+            int tal1 = GetInput();
+            Console.Write("Tal 2: ");
+            int tal2 = GetInput();
+            Console.Write("Summan blir: ");
+            return tal1 - tal2;
+        }
+
+        static int Multiplikation() //Skapar sektion för multiplikation 
+        {
+            Console.Write("Tal 1: ");
+            int tal1 = GetInput();
+            Console.Write("Tal 2: ");
+            int tal2 = GetInput();
+            Console.Write("Summan blir: ");
+            return tal1 * tal2;
+            
+        }
+
+
+
+    }
+}
