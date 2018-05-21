8c6a15043b86cac27b9c646d9eed0710226f5d0f
ntijones <31617373+ntijones@users.noreply.github.com>
2018-05-21 12:48:33 +0200

    Multiplikationstabbellen

---
+++ Multiplikationstabbellen/Multiplikationstabbellen/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multiplikationstabbellen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Skriver ut till programmet.
+            Console.WriteLine("Multiplikationstabeller");
+            Console.Write("Skriv in ett heltal:");
+            //Ger userInput ett värde sedan gör int.Parse så att programmet inte stänger ner sig. 
+            int userInput = int.Parse(Console.ReadLine());
+            //Ger räknaren variblen i och ett värde.
+            for (int i = 1; i < 11; i++)
+            {
+                //Multiplicerar int produkt, (användarens värde) och räknaren
+                int produkt = userInput * i;
+                Console.WriteLine(produkt);
+            }
+            Console.ReadLine();
+        }
+    }
+}
